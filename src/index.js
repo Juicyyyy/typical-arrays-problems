578fbf6346423c67b6a5a2afdbd6737196590697
Julia <diniullova2011@yandex.ru>
2021-08-18 15:42:52 +0400

    tipical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 
 exports.min = function min (array) {
-  return 0;
+    
+    if ((array.length == 0) && (array.length == [])){ return 0; }
+    else {
+        var min = parseInt(array[array.length-1]), el;
+        for(var i=array.length-2; i>=0; i--){
+            el = parseInt(array[i]);
+            if(el<min){
+                min = el;
+            }
+        }
+        return min;
+    }
 }
 
 exports.max = function max (array) {
-  return 0;
+    
+    if (array == 0) { return 0; }
+    else {
+        var max = parseInt(array[array.length-1]), el;
+        for(var i=array.length-2; i>=0; i--){
+            el = parseInt(array[i]);
+            if(el>max){
+                max = el;
+            }
+        }
+        return max;
+    }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == 0) { return 0; }
+    else {
+        let sum = 0;
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    res = sum / array.length;
+    return res;
+    }   
 }
